264ce263daf39642021e9e45326e15c742d1215b
saiman4113 <saiman.cse.20230104113@aust.edu>
2025-07-26 16:23:13 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <head>hello</head>
+  );
+}
+
+export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,8 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+const home = () => {
+    return ( 
+        <head></head>
+     );
+}
+ 
+export default home;
